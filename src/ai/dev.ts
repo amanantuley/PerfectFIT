30454b9f98ddaf5f3512277e8398eb3e1c65b09c
Aman Antuley <160766309+amanantuley@users.noreply.github.com>
2025-08-03 09:40:18 +0530

    Delete src directory

--- src/ai/dev.ts
+++
@@ -1,8 +0,0 @@
-'use server';
-import { config } from 'dotenv';
-config();
-
-import '@/ai/flows/extract-body-measurements.ts';
-import '@/ai/flows/recommend-garments.ts';
-import '@/ai/flows/chat-with-perfect-ai.ts';
-import '@/ai/flows/generate-fitness-plan.ts';
