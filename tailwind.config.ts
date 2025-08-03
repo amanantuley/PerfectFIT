af62997157e5201447e7ee429b3d8561f9aaf1a6
Aman Antuley <160766309+amanantuley@users.noreply.github.com>
2025-08-03 12:23:15 +0530

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -75,9 +75,6 @@ export default {
         md: 'calc(var(--radius) - 2px)',
         sm: 'calc(var(--radius) - 4px)',
       },
-       backgroundSize: {
-        'size-200': '200% 200%',
-      },
       keyframes: {
         'accordion-down': {
           from: {
@@ -105,20 +102,17 @@ export default {
             transform: 'translateY(0)',
           },
         },
-        rainbow: {
-          '0%, 100%': {
-            'background-position': '0% 50%',
-          },
-          '50%': {
-            'background-position': '100% 50%',
-          },
+        'text-rainbow': {
+          '0%': { backgroundPosition: '0% 50%' },
+          '50%': { backgroundPosition: '100% 50%' },
+          '100%': { backgroundPosition: '0% 50%' },
         },
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
         'fade-in-up': 'fade-in-up 0.8s ease-out forwards',
-        rainbow: 'rainbow 6s ease-in-out infinite',
+        'text-rainbow': 'text-rainbow 3s ease-in-out infinite',
       },
     },
   },
