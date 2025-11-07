101f09ebe415f1d9928eab43e3ac3de4ff7f6956
Aman Antuley <amanantuley3794@gmail.com>
2025-11-07 15:00:09 +0530

    ..

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,23 +1,28 @@
 import { initializeApp } from "firebase/app";
-import { 
-  getAuth, 
-  GoogleAuthProvider, 
-  RecaptchaVerifier, 
-  signInWithPhoneNumber 
+import {
+  getAuth,
+  GoogleAuthProvider,
+  RecaptchaVerifier,
+  signInWithPhoneNumber,
 } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBG2zB42UJ1b8WAxLV3CLkdBaGAP-jCqM4",
   authDomain: "perfectfit-b5581.firebaseapp.com",
   projectId: "perfectfit-b5581",
-  storageBucket: "perfectfit-b5581.appspot.com",   // ✅ fixed
+  storageBucket: "perfectfit-b5581.appspot.com",
   messagingSenderId: "459674943880",
   appId: "1:459674943880:web:387c759500868d02943a3d",
-  measurementId: "G-8N9TX7MB19"
+  measurementId: "G-8N9TX7MB19",
 };
 
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const googleProvider = new GoogleAuthProvider();
 
-export { auth, googleProvider, RecaptchaVerifier, signInWithPhoneNumber };
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
+export const googleProvider = new GoogleAuthProvider();
+export { RecaptchaVerifier, signInWithPhoneNumber };
+export default app;
